233470dc5d06ad45c973d028d252bcb9aa91ade5
mbmc7443 <mbmc2004@naver.com>
2019-08-26 13:58:52 +0900

    구글 로그인

---
+++ public/js/note.js
@@ -0,0 +1,44 @@
+const auth = firebase.auth();
+const google = new firebase.auth.GoogleAuthProvider();
+var user = null;	// 로그인한 사용자의 정보를 저장하는 변수
+
+// 인증기능 만들기
+// $("#btLogin").click(function(e){});
+auth.onAuthStateChanged(data => {
+    console.log(data);
+    user = data;
+    console.log(user);
+    if(user == null) viewChg('');
+    else viewChg('R');
+});
+document.querySelector("#btLogin").addEventListener("click", e => {
+	auth.signInWithPopup(google);  // 팝업창이 떠서 로그인할수있는것
+    //auth.signInWithRedirect(google); 구글사이트로 넘어가는것
+});
+document.querySelector("#btLogout").addEventListener("click", e => {
+	auth.signOut();
+});
+
+
+
+//화면 전환 함수
+function viewChg(state){
+    switch(state){
+        case "R":
+            console.log(user.photoURL);
+            if(user.photoURL) url =user.photoURL;
+            else url ="";
+            if(user.photoURL!=undefined)
+                document.querySelector(".email img").setAttribute("src",url);
+            document.querySelector(".email-txt").innerHTML=user.email;
+            document.querySelector(".email").style.display="flex";
+            document.querySelector("#btLogin").style.display="none";
+            break;
+        default:
+                document.querySelector(".email-txt").innerHTML="";
+                document.querySelector(".email").style.display="none";
+                document.querySelector("#btLogin").style.display="flex";
+            break;    
+    }
+    
+}
---
+++ public/js/util.js
@@ -0,0 +1,39 @@
+// '2019년 8월 11일 9시 8분 11초'형식으로 보내주는 함수
+function dspDate(d,type=1){
+    var monthArr=["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월"]
+    var year = d.getFullYear()+"년"; // 현재 년도 (2019)
+    var month = monthArr[d.getMonth()]+[]; // 현재월-1 (0~11) 배열이기때문이다
+    var day = d.getDate()+"일"; //현재 일  (1~31)
+    var hour = d.getHours()+"시"; //현재시간 (0~23)
+    var min = d.getMinutes()+"분"; //현재분 (0~59)
+    var sec = d.getSeconds();+"초"; //현재초 (0~59)
+    var returnStr;
+    /* 
+    type 0 : '2019년 8월 11일 9시 8분 11초'
+    type 1 : '2019년 8월 11일 9시 8분'
+    type 2 : '2019년 8월 11일 9시'
+    type 3 : '2019년 8월 11일'
+    type 4 : '8월 11일'
+    */
+    switch(type){
+       case 1:
+           returnStr = year +month +day +hour +min;
+           break;
+       case 2:
+           returnStr = year +month +day +hour;
+           break;
+       case 3:
+           returnStr = year +month +day;
+           break;
+       case 4:
+           returnStr = month +day;
+           break;
+       default:
+           returnStr = year +month +day +hour +min +sec;
+           break;
+    }
+
+
+
+     return returnStr;
+}
